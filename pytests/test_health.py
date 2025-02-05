5bb9cee177741af3e4e74305f2c10a33c8fba63e
Darvin <61823788+darvin00@users.noreply.github.com>
2025-02-05 15:13:45 +0530

    Update test_health.py

--- pytests/test_health.py
+++ pytests/test_health.py
@@ -2,5 +2,5 @@
 import requests
 
 def test_health():
-    response = requests.get('http://18.236.161.197:8000/health')  
+    response = requests.get('http://54.149.140.148:8000/health')  
     assert response.status_code == 200
