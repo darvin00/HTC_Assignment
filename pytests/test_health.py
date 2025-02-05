02927d54e97856f620ac935bf436832f2cc04678
root <root@ip-172-31-22-110.us-west-2.compute.internal>
2025-02-05 06:04:18 +0000

    update

--- pytests/test_health.py
+++ pytests/test_health.py
@@ -2,5 +2,5 @@
 import requests
 
 def test_health():
-    response = requests.get('http://34.212.182.222:5000/health')  
+    response = requests.get('http://34.212.25.129:5000/health')  
     assert response.status_code == 200
